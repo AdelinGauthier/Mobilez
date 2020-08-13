7338ae87cd94f9e8ea9ee76d4526bbb224a1d9b0
AdelinGauthier <63398248+AdelinGauthier@users.noreply.github.com>
2020-08-13 03:56:38 +0200

    link to new app

--- accueil/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from accueil import views
-
-urlpatterns = [
-    # mobilez.be/accueil
-    path('', views.accueil_index, name="accueil"),
-    # mobilez.be/accueil/#numeroPK
-    path("<int:pk>/", views.accueil_detail, name="accueil_detail"),
-]
